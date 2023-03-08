1f1e752f25637bc7a60f8794ba084f96843b0db2
githubwqj2 <2812930196@qq.com>
2023-03-08 10:53:18 +0800

    111

---
+++ controller.py
@@ -0,0 +1,35 @@
+import click
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+# the minimal Flask application
+@app.route('/')
+def index():
+    return render_template('index.html',dataxxx='zheshi chuancanlede')
+
+
+# bind multiple URL for one view function
+@app.route('/hi')
+@app.route('/hello')
+def say_hello():
+    return '<h1>Hello, Flask!</h1>'
+
+
+# dynamic route, URL variable default
+@app.route('/greet', defaults={'name': 'Programmer'})
+@app.route('/greet/<name>')
+def greet(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
+# custom flask cli command
+@app.cli.command()
+def hello():
+    """Just say hello."""
+    click.echo('Hello, Human!')
+
+
+if __name__ == '__main__':
+    app.run(port=5000,host="127.0.0.1",debug=True)
