89b0ce47c5598a685e89c3980efe4b2ed0b2cc70
githubwqj2 <2812930196@qq.com>
2023-04-21 17:59:47 +0800

    添加问答

--- elasticsearchdao.py
+++ elasticsearchdao.py
@@ -1,25 +1,10 @@
-import pymysql
-from sqlalchemy import create_engine
-# from sqlalchemy.dialects.mysql import pymysql
-from elasticsearch import Elasticsearch, helpers
-import pojo
 
-conn = pymysql.Connect(
-    host='47.120.39.188',
-    port=3306,
-    user='root',
-    passwd='qwertyuiop123456',
-    db='relic_data',
-    charset='utf8'
-)
-cursor = conn.cursor()
-engine = create_engine("mysql+pymysql://root:123456@192.168.4.110:3306/relic_data?charset=utf8")
+from elasticsearch import Elasticsearch, helpers
+
+es = Elasticsearch([{"host": "47.120.39.188", "port": 3306, "timeout": 1500}])
 
 
 def read_es(host, port, index, query=""):
-    url = {"host": host, "port": port, "timeout": 1500}
-
-    es = Elasticsearch([url])
     if es.ping():
         print("Successfully connect!")
     else:
@@ -36,52 +21,16 @@ def read_es(host, port, index, query=""):
     return res
 
 
-from elasticsearch import Elasticsearch
-from elasticsearch.helpers import bulk
 
 
-def write_es(host, port, index, query=""):
-    es = Elasticsearch([{"host": host, "port": port, "timeout": 1500}])
-
-    ACTIONS = []
-    action1 = {
-        "_index": "indes_test",
-        "_type": "doc_type_test",
-        "_id": "bSlegGUBmJ2C8ZCSC1R1",
-        "_source": {
-            "id": "1111122222",
-            "serial": "版本",
-            "tags.content": "标签2",
-            "tags.dominant_color_name": "域名的颜色黄色",
-            "tags.skill": "分类信息",
-            "hasTag": "123",
-            "status": "11",
-            "createTime": "2018-2-2",
-            "updateTime": "2018-2-3",
-        }
-    }
-    action2 = {
-        "_index": "indes_test",
-        "_type": "doc_type_test",
-        "_id": "bSlegGUBmJ2C8ZCSC1R2",
-        "_source": {
-            "id": "1111122222",
-            "serial": "版本",
-            "tags.content": "标签2",
-            "tags.dominant_color_name": "域名的颜色黄色",
-            "tags.skill": "分类信息",
-            "hasTag": "123",
-            "status": "11",
-            "createTime": "2018-2-2",
-            "updateTime": "2018-2-3",
-        }
-    }
-
-    ACTIONS.append(action1)
-    ACTIONS.append(action2)
-
-    res, _ = bulk(es, ACTIONS, index="indes_test", raise_on_error=True)
-    print(res)
 
 
 data = read_es('47.120.39.188', 9200, "fasss.index", query="")
+
+# 查询es
+body = {
+    "query":{
+        "match_all":{}
+    }
+}
+es.search(index="culture_heritage" ,body=body)
